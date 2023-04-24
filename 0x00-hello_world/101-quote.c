f0f3725ca649a8fb5f116994c157b7ff6f80e721
Holladworld <oladimejiolayinka95@gmail.com>
2023-04-24 14:13:40 -0700

    A c program that prints a line to the standard output

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - A c program that print a line to standard error
- * Return 1 (Success)
+ * Return: Always 1
  */
- int main (void)
+int main (void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return(1);
+	write(2 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
