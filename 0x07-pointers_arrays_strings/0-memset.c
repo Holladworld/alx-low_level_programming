792b31cc7321e687b5e0c1be90870abb10c4279a
Holladworld <oladimejiolayinka95@gmail.com>
2023-05-08 10:01:19 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * memset - fills memory with constant byte.
+ * @s: poniter to put the constant
+ * @b: constant ( the desired value)
+ * @n: max bytes to use i.e the number of bytes to be changed
+ * Return: changed array with the new value(constant) for n bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; n > 0; i++, n--)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
