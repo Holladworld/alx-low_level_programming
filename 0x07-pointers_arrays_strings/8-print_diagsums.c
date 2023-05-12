77fe1e55d266ff0b855f170ca351d1b2b7a84f4e
Holladworld <oladimejiolayinka95@gmail.com>
2023-05-12 10:42:23 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,7 +8,7 @@
  * @size: input, number matrix
  * Return: 0
  */
-void print_diagsums(in *a, int size)
+void print_diagsums(int *a, int size)
 {
 	/*Declaring the variables*/
 	int q;
@@ -18,7 +18,7 @@ void print_diagsums(in *a, int size)
 	sum_2 = 0;
 
 	q = 0;
-	while (i < size) /*number repetitions*/
+	while (q < size) /*number repetitions*/
 	{
 		/*sum for diagsums*/
 		sum_1 = sum_1 + *(a + q * size + q);
