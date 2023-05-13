98cc88d75d67df93194870287d7205cba78235d5
Holladworld <oladimejiolayinka95@gmail.com>
2023-05-13 04:28:31 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -75,7 +75,11 @@ int wildcmp(char *s1, char *s2)
 		iterate_wild(&s2);
 		s2 = matched_postfix(s1, s2);
 	}
+	if (*s2 == '\0')
+		return (1);
+
 	if (*s1 != *s2)
 		return (0);
+
 	return (wildcmp(++s1, ++s2));
 }
