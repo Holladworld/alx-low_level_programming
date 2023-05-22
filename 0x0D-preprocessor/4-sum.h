965372ce22debdd6a4867526fd77ff76ec7e541b
Holladworld <oladimejiolayinka95@gmail.com>
2023-05-22 06:54:08 -0700

    4-sum.h

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,9 @@
+#ifndef SUM_H
+#define SUM_H
+/*
+ * file: 4-sum.h
+ * desc: Header file that defines a function-like macro
+ * SUM(x, y) computing the sum of x and y
+ */
+#define SUM(x, y) ((x) + (y))
+#endif
