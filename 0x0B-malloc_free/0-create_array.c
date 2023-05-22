074db4018564e89717ba37ebcdb02567b97988c0
Holladworld <oladimejiolayinka95@gmail.com>
2023-05-22 08:49:30 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,19 +9,20 @@
  * Return: if size == 0 or function fails - NULL
  * otherwise - a pointer to the array
  */
-char *create_array(unsigned in size, char c)
+char *create_array(unsigned int size, char c)
 {
 	char *array;
 	unsigned int entry;
 
 	if (size == 0)
 		return (NULL);
+
 	array = malloc(sizeof(char) * size);
 
 	if (array == NULL)
 		return (NULL);
 
-	for (entry = 0; index < size; entry++)
+	for (entry = 0; entry < size; entry++)
 		array[entry] = c;
 	return (array);
 }
