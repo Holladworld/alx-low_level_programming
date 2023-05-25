af09b8f0d116ca1c35ee601b8fa1a7223b08d47f
Holladworld <oladimejiolayinka95@gmail.com>
2023-05-25 10:41:59 -0700

    function that carry out a function on each element of array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,8 +8,9 @@
  * @action: pointer to print in regular or hex
  *
  */
-void array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
+
 	if (array == NULL || action == NULL)
 		return;
 
