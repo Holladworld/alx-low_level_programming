e813bb063c3e201a3d613f21bc9f06c654bfc66b
Holladworld <oladimejiolayinka95@gmail.com>
2023-05-25 09:42:16 -0700

    function that prints name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,10 +6,11 @@
  * print_name - function to print a name
  * @name: name to be printed
  * @f: function pointer
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (f == NULL)
+	if (f == NULL || name == NULL)
 		return;
 
 	f(name);
