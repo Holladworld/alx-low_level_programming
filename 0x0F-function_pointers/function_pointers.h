c000a9551fbc7093704a9ae9c9c50ae8e60870f6
Holladworld <oladimejiolayinka95@gmail.com>
2023-05-25 10:04:43 -0700

    function that prints name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,12 +2,14 @@
 #define FUNCTION_POINTERS_H
 
 /**
- * Desc: Header file containing prototypes of all functions for the task 0x0F- function_pointers directory
+ * Desc: Heade file containing prototypes of all functions for the task 0x0F- function_pointers directory
  */
 
 #include <stdlib.h>
-
-void print_name(char *name, void (*f)(char *))
-int _putchar(char c);
+#include <stdio.h>
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+int (*get_op_func(char *s))(int, int);
 
 #endif
