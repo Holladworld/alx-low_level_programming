43e09054dd4908e0190fd07d95a291bdabde47b3
Holladworld <oladimejiolayinka95@gmail.com>
2023-05-26 06:25:42 -0700

    function that returns the sum of all its parameters

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,13 +2,25 @@
 #define VARIADIC_FUNCTIONS_H
 
 /**
- * File: variadic_functions.h
- * Desc: prototypes for all functions used in the directory
+ * file: variadic_functions.h
+ * desc: header  file containing  prototypes for all functions
+ * used in the directory
  * 0x10-variadic_function directory
  */
 
 #include <stdarg.h>
 #include <stdio.h>
+/*struct printer - A new struct type defining a printer
+ * @symbol: represent data type
+ * @print: point to a function thar prints a data type corresponding to symbol
+ */
+typedef struct printer
+{
+	char *symbol;
+	void(*print)(va_list arg);
+
+} printer_t;
+
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
