2f241d13f2531067c384170f56788f48f4b0875d
holladworld <oladimejiolayinka95@gmail.com>
2023-06-09 08:05:25 -0700

    function that count legnth of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion - function to print string length
+ * @s: string to be counted
+ * Return: 0
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
+}
