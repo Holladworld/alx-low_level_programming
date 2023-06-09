23e97bb1e43c7b097c520ef07278a84319280b75
holladworld <oladimejiolayinka95@gmail.com>
2023-06-09 13:53:49 -0700

    function to check palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * string_length - check length of string
+ * @s: string to be checked
+ * Return: length of string
+ */
+int string_length(char *s)
+{
+	if (*s == '\0')
+	{
+		return (1);
+	}
+	else
+	{
+	return (1 + string_length(++s));
+	}
+}
+/**
+ * recursive - recursive check of palindrome
+ * @s: string checked
+ * @len: length of the string
+ * Return: 0 if not palindrome
+ */
+int recursive(char *s, int len)
+{
+	if (len <= 0)
+		{
+			return (1);
+		}
+		else if (*s == *(s + len))
+		{
+			return (recursive(++s, len - 2));
+		}
+	else
+	{
+		return (0);
+	}
+}
+/**
+ * is_palindrome - function to check if string is palindrome
+ * @s: pointer to string
+ * Return: recursion
+ */
+int is_palindrome(char *s)
+{
+	int len = string_length(s) - 1;
+
+	return (recursive(s, len - 1));
+}
