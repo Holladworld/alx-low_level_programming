21874ccf9fde63480935c9e698aa1ddca442a7c8
holladworld <oladimejiolayinka95@gmail.com>
2023-06-17 07:56:06 -0700

    function to multiply two positive number

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * main - function that multipies two numbers
+ * @argv: argument vector
+ * @argc: the number of arguments
+ * Return: error if arguments is incorrect
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	unsigned long mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (num1 = 1; num1 < argc; num1++)
+	{
+		for (num2 = 0; argv[num1][num2] != '\0'; num2++)
+		{
+			if (argv[num1][num2] > 57 || argv[num1][num2] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+
+	mul = atoi(argv[1]) * atoi(argv[2]);
+	printf("%lu\n", mul);
+	return (0);
+}
