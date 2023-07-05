ab223180e9c5a3fc07e9f4b414a766ed848f5807
holladworld <oladimejiolayinka95@gmail.com>
2023-07-05 08:16:18 -0700

    function that free list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees linstint lists
+ * @head: the head od the list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (!head)
+	{
+		return;
+	}
+	if (*head)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+}
